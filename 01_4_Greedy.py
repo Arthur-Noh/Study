f601eeb9861682a9541dddc7cd08fe3f098a8b2b
노동준 <88483892+Arthur-Noh@users.noreply.github.com>
2021-09-24 20:29:17 +0900

    Create 01_4_Greedy.py

---
+++ 01_4_Greedy.py
@@ -0,0 +1,17 @@
+# 그리디 알고리즘 no.4
+# 모험가 길드
+
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()  # 오름차순 정렬 수행
+
+result = 0  # 총 그룹의 수
+count = 0  # 현재 그룹에 포함된 모험가의 수
+
+for i in data:
+    count += 1  # 현재 그룹에 해당 모험가를 포함 시키기
+    if count >= i:  # 현재 그룹에 포함된 모험가의 수가 현재 공포도 이상이라면, 그룹을 결성
+        result += 1  # 총 그룹의 수 증가시키기
+        count = 0  # 현재 그룹에 포함된 모험가의 수 초기화
+
+print(result)  # 총 그룹의 수 출력

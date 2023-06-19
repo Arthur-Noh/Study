9e52320d638822b2213db2c2bf89faa98cf297ea
Arthur-Noh <dkagod97@gmail.com>
2023-06-19 14:38:57 +0900

    feat: section5 of 7, 8

--- javascript/section5/7.js
+++ javascript/section5/7.js
@@ -1,5 +1,40 @@
-const solution7 = () => {
-    let answer;
+// 아나그램(해쉬)
+// Anagram이란 두 문자열이 알파벳의 나열 순서를 다르지만 그 구성이 일치하면 두 단어는 아 나그램이라고 합니다.
+// 예를 들면 AbaAeCe 와 baeeACA 는 알파벳을 나열 순서는 다르지만 그 구성을 살펴보면 A(2), a(1), b(1), C(1), e(2)로 알파벳과 그 개수가 모두 일치합니다. 즉 어느 한 단어를 재 배열하면 상대편 단어가 될 수 있는 것을 아나그램이라 합니다.
+// 길이가 같은 두 개의 단어가 주어지면 두 단어가 아나그램인지 판별하는 프로그램을 작성하세 요. 아나그램 판별시 대소문자가 구분됩니다.
+// ▣ 입력설명
+// 첫 줄에 첫 번째 단어가 입력되고, 두 번째 줄에 두 번째 단어가 입력됩니다. 단어의 길이는 100을 넘지 않습니다.
+// ▣ 출력설명
+// 두 단어가 아나그램이면 “YES"를 출력하고, 아니면 ”NO"를 출력합니다.
+
+const solution7 = (s1='', s2='') => {
+    let answer = 'YES';
+    let s1Hash = new Map();
+    let s2Hash = new Map();
+
+    for (let i = 0 ; i < s1.length ; i++) {
+        if (s1Hash.has(s1[i])) {
+            s1Hash.set(s1[i], s1Hash.get(s1[i]) + 1);
+        } else {
+            s1Hash.set(s1[i], 1);
+        }
+        if (s2Hash.has(s2[i])) {
+            s2Hash.set(s2[i], s2Hash.get(s2[i]) + 1);
+        } else {
+            s2Hash.set(s2[i], 1);
+        }
+    }
+
+    for (let [key, value] of s1Hash) {
+        if (!s2Hash.has(key)) {
+            answer = 'NO';
+            break;
+        }
+        if (s2Hash.get(key) !== value) {
+            answer = 'NO';
+            break;
+        }
+    }
 
     return answer;
 };
--- javascript/section5/8.js
+++ javascript/section5/8.js
@@ -1,5 +1,70 @@
-const solution8 = () => {
-    let answer;
+// 모든 아나그램 찾기 (해쉬, 투포인터, 슬라이딩 윈도우)
+// S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하 세요. 아나그램 판별시 대소문자가 구분됩니다. 부분문자열은 연속된 문자열이어야 합니다.
+// ▣ 입력설명
+// 첫 줄에 첫 번째 S문자열이 입력되고, 두 번째 줄에 T문자열이 입력됩니다.
+// S문자열의 길이는 10,000을 넘지 않으며, T문자열은 S문자열보다 길이가 작거나 같습니다.
+// ▣ 출력설명
+// S단어에 T문자열과 아나그램이 되는 부분문자열의 개수를 출력합니다.
+// ▣ 입력예제 1 bacaAacba abc
+// ▣ 출력예제 1 3
+// 출력설명: {bac}, {acb}, {cba} 3개의 부분문자열이 "abc"문자열과 아나그램입니다.
+
+const compareMaps = (map1, map2) => {
+    if (map1.size !== map2.size ) {
+        return false; // key 종류, 갯수 자체가 다르기 때문에
+    }
+
+    for (let [key, value] of map1) {
+        if (!map2.has(key)) {
+            return false;
+        }
+        if (map2.get(key) !== value) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+const solution8 = (s='', t='') => {
+    let answer = 0;
+    let sHash = new Map();
+    let tHash = new Map();
+    let length = t.length - 1;
+
+    for (let x of t) {
+        if (tHash.has(x)) {
+            tHash.set(x, tHash.get(x) + 1);
+        } else {
+            tHash.set(x, 1);
+        }
+    }
+
+    for (let i = 0 ; i < length ; i++) {
+        if (sHash.has(s[i])) {
+            sHash.set(s[i], sHash.get(s[i]) + 1);
+        } else {
+            sHash.set(s[i], 1);
+        }
+    }
+
+    let left = 0;
+    for (let right = length ; right < s.length ; right++ ) {
+        if (sHash.has(s[right])) {
+            sHash.set(s[right], sHash.get(s[right]) + 1);
+        } else {
+            sHash.set(s[right], 1);
+        }
+
+        if (compareMaps(sHash, tHash)) {
+            answer ++;
+        }
+        sHash.set(s[left], sHash.get(s[left]) - 1);
+        if (sHash.get(s[left]) === 0) {
+            sHash.delete(s[left]);
+        }
+        left++;
+    }
 
     return answer;
 };

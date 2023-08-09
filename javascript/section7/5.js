741c2d70978e6972420d6b878ac94f8a40912b63
Arthur-Noh <dkagod97@gmail.com>
2023-08-09 18:04:58 +0900

    section7: 5, 6

--- javascript/section7/5.js
+++ javascript/section7/5.js
@@ -1,5 +1,37 @@
+// Least Recently Used(카카오 캐시 문제 변형)
+
+// 삽입정렬로 해결해보자.
 const solution5 = () => {
-    let answer;
+    const size = 5;
+    let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
+    let answer = Array.from({ length: size }, () => 0);
+
+    arr.forEach(value => {
+        let position = -1;
+
+        // 배열에 이미 값이 존재하는지 확인한다.
+        for (let i = 0 ; i < size ; i++) {
+            if (value === answer[i]) {
+                position = i;
+                break;
+            }
+        }
+
+        // 만약 배열 안에 값이 존재하지 않는다면
+        if (position === -1) {
+            for (let i = size - 1 ; i > 0 ; i--) {
+                answer[i] = answer[i - 1];
+            }
+            answer[0] = value;
+        }
+        // 만약 배열 안에 값이 존재한다면
+        else {
+            for (let i = position ; i > 0 ; i--) {
+                answer[i] = answer[i - 1];
+            }
+            answer[0] = value;
+        }
+    });
 
     return answer;
 };
--- javascript/section7/6.js
+++ javascript/section7/6.js
@@ -1,5 +1,16 @@
+// 장난꾸러기 현수
+// 정렬을 사용하면 더 쉽게 해결할 수 있다.
+// 정렬을 해서 원본 배열과 위치가 다른 친구 2개를 찾으면 된다.
 const solution6 = () => {
-    let answer;
+    let arr = [120, 125, 152, 130, 135, 135, 143, 127, 160];
+    let sortedArr = arr.slice().sort((a, b) => a - b);
+    let answer = [];
+
+    sortedArr.map((value, index) => {
+        if (value !== arr[index]) {
+            answer.push(index + 1);
+        }
+    });
 
     return answer;
 };

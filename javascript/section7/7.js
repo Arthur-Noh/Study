097211db3c734107352ccdc7a79267d121edb065
Arthur-Noh <dkagod97@gmail.com>
2023-08-11 13:39:25 +0900

    section7: 7, 8

--- javascript/section7/7.js
+++ javascript/section7/7.js
@@ -1,5 +1,20 @@
+// 좌표 정렬
 const solution7 = () => {
-    let answer;
+    let arr = [
+        [2, 7],
+        [1, 3],
+        [1, 2],
+        [1, 2],
+        [2, 5],
+        [3, 6],
+    ];
 
-    return answer;
+    arr.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return a[1] - b[1];
+        }
+        return a[0] - b[0];
+    });
+
+    return arr;
 };
--- javascript/section7/8.js
+++ javascript/section7/8.js
@@ -1,5 +1,33 @@
-const solution8 = () => {
-    let answer;
+// 회의실 배정
 
+// 그리디 알고리즘으로 해결할 수 있다.
+// 끝나는 시간을 기준으로 정렬하고, 같을 경우에는 앞 시간을 비교해서 정렬하면 된다.
+const solution8 = () => {
+    let meeting = [
+        [1, 4],
+        [2, 3],
+        [3, 5],
+        [4, 6],
+        [5, 7],
+    ];
+    let answer = 0;
+
+    meeting.sort((a, b) => {
+        if (a[1] === b[1]) {
+            return a[0] - b[0];
+        }
+        else {
+            return a[1] - b[1];
+        }
+    });
+
+    let endTime = 0;
+
+    for (let x of meeting) {
+        if (x[0] >= endTime) {
+            answer++;
+            endTime = x[1];
+        }
+    }
     return answer;
 };

41351d16142e05cddec5c50ac58224419991cfcf
Arthur-Noh <dkagod97@gmail.com>
2023-08-14 11:20:12 +0900

    section7: 9, 10

--- javascript/section7/10.js
+++ javascript/section7/10.js
@@ -1,5 +1,29 @@
+// 이분검색
 const solution10 = () => {
+    let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+    let target = 32;
     let answer;
+
+    // 오름차순 정렬을 수행한다.
+    arr.sort((a, b) => a - b);
+
+    // 이분 탐색을 수행한다.
+    let left = 0;
+    let right = arr.length - 1;
+    while(left <= right) {
+        let middle = parseInt((left + right) / 2);
+
+        if (arr[middle] === target) {
+            answer = middle + 1;
+            break;
+        }
+        else if (arr[middle] > target) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
 
     return answer;
 };
--- javascript/section7/9.js
+++ javascript/section7/9.js
@@ -1,5 +1,43 @@
+// 결혼식
 const solution9 = () => {
-    let answer;
+    let answer = Number.MIN_SAFE_INTEGER;
+    let times = [
+        [14, 18],
+        [12, 15],
+        [15, 20],
+        [20, 30],
+        [5, 14],
+    ];
+
+    // 각각의 시간 라인을 넣어준다.
+    let timeLine = [];
+    for (let time of times) {
+        timeLine.push([time[0], 's']);
+        timeLine.push([time[1], 'e']);
+    }
+
+    // 시간을 정렬해준다.
+    timeLine.sort((a, b) => {
+        // 아스키코드상 e가 더 낮으므로 오름차순 정렬
+        // 나가는게 먼저고 들어오는걸 계산하기 위해서
+        // 들어오고 나가게 된다면 갯수를 1개 더 세기 때문이다.
+        if(a[0] === b[0]) {
+            return a[1].charCodeAt() - b[1].charCodeAt();
+        }
+        return a[0] - b[0];
+    });
+
+    // 갯수를 세어준다.
+    let count = 0;
+    for (let x of timeLine) {
+        if (x[1] === 's') {
+            count++;
+        } else {
+            count--;
+        }
+        
+        answer = Math.max(answer, count);
+    }
 
     return answer;
 };
